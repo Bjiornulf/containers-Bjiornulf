898b47e8c4dd3dd815fc40e21b61a639b817720d
Bjiornulf <tpeev01@gmail.com>
2021-09-15 12:02:33 +0200

    added methods templates

--- src/container/IntPriorityQueue.java
+++ src/container/IntPriorityQueue.java
@@ -2,6 +2,45 @@ package container;
 
 import java.util.Iterator;
 
-public class IntPriorityQueue extends Queue<Integer> {
+/**
+ * Implements Integer specific priority Queue
+ * 
+ * @author Todor PEEV
+ */
+public class IntPriorityQueue implements Queue<Integer> {
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+
+    @Override
+    public boolean insertElement(Integer e) {
+        return false;
+    }
+
+    @Override
+    public Integer element() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Integer popElement() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
 }
