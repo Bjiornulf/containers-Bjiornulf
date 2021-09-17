ee979cf654bdb64cb2ceca9c41875b25690e3fd6
Bjiornulf <tpeev01@gmail.com>
2021-09-17 14:44:32 +0200

    implemented insertion

--- src/container/IntPriorityQueue.java
+++ src/container/IntPriorityQueue.java
@@ -1,6 +1,9 @@
 package container;
 
+import java.util.Currency;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
  * Implements Integer specific priority Queue
@@ -9,20 +12,62 @@ import java.util.Iterator;
  */
 public class IntPriorityQueue implements Queue<Integer> {
 
+    private Integer[] array;
+    private int size;
+
+    public IntPriorityQueue(int capacity) {
+        this.array = new Integer[capacity];
+        this.size = 0;
+    }
+
+    private void doubleSize() {
+        Integer[] newArray = new Integer[this.size*2];
+        for (int i = 0; i < this.array.length; i++) {
+            newArray[i] = this.array[i];
+        }
+        this.array = newArray;
+    }
+
     @Override
     public Iterator<Integer> iterator() {
         return null;
     }
 
+    public String toString() {
+        String toReturn = "";
+        for (int i = 0; i < this.size; i++) {
+            toReturn += this.array[i] + ":";
+        }
+        return toReturn;
+    }
+
     @Override
     public boolean insertElement(Integer e) {
-        return false;
+        if (this.size == this.array.length){
+            this.doubleSize();
+        }
+        this.array[this.size++] = e;
+        int currentIndex = this.size - 1;
+        int ancestorIndex = (currentIndex + 1) / 2 - 1;
+        Integer temp;
+        while (currentIndex > 0 && this.array[currentIndex] > this.array[ancestorIndex]) {
+            temp = this.array[ancestorIndex];
+            this.array[ancestorIndex] = this.array[currentIndex];
+            this.array[currentIndex] = temp;
+
+            currentIndex = ancestorIndex;
+            ancestorIndex = (currentIndex + 1) / 2 - 1;
+        }
+            depth--;
+            return true;
     }
 
     @Override
     public Integer element() {
-        // TODO Auto-generated method stub
-        return null;
+        if (this.size == 0) {
+            throw new NoSuchElementException("Queue empty");
+        }
+        return this.array[0];
     }
 
     @Override
