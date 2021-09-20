5e7ec8a252504290b6f0ae8f696871f4c7fa9d68
Bjiornulf <tpeev01@gmail.com>
2021-09-20 11:13:34 +0200

    fix popElement()

--- src/container/GenPriorityQueue.java
+++ src/container/GenPriorityQueue.java
@@ -100,7 +100,7 @@ public class GenPriorityQueue<E extends Comparable<E>> implements Queue<E> {
         int childIndex1 = 2 * currentIndex + 1;
         while (childIndex1 < this.size) {
             if (childIndex1 < this.size - 1) {
-                childIndex1 = this.array[childIndex1].compareTo(this.array[childIndex1]) > 0 ? childIndex1 : childIndex1 + 1;
+                childIndex1 = this.array[childIndex1].compareTo(this.array[childIndex1+1]) > 0 ? childIndex1 : childIndex1 + 1;
             }
             if (this.array[currentIndex].compareTo(this.array[childIndex1]) > 0) {
                 break;
